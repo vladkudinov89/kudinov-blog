bfe95a6811342a608b50bc55b01c0d57a170c05a
vladkudinov89 <vlad.kudinov.89@gmail.com>
2018-06-02 20:30:52 +0300

    Add logic to Models. Add slugabble to models(Category,post,tag)

--- app/Post.php
+++ app/Post.php
@@ -3,8 +3,46 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 class Post extends Model
 {
-    //
+
+    use Sluggable;
+
+    public function category()
+    {
+        return $this->hasOne(Category::class);
+    }
+
+    public function author()
+    {
+        return $this->hasOne(User::class);
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany(
+            Tag::class,
+            'post_tags',
+            'post_id',
+            'tag_id'
+        );
+    }
+
+    /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    public function sluggable()
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
+
+
 }
