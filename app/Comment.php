c68970ca2cf25fad24449cdbb9f9deb26b64ea74
vladkudinov89 <vlad.kudinov.89@gmail.com>
2018-06-04 17:05:48 +0300

    Description models for projects. Add default path to uploade images (blog && user)

--- app/Comment.php
+++ app/Comment.php
@@ -6,11 +6,40 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-   public function post(){
-       return $this->hasOne(Post::class);
-   }
+    public function post()
+    {
+        return $this->hasOne(Post::class);
+    }
 
-   public function author(){
-       return $this->hasOne(User::class);
-   }
+    public function author()
+    {
+        return $this->hasOne(User::class);
+    }
+
+    public function allow()
+    {
+        $this->status = 1;
+        $this->save();
+    }
+
+    public function disAllow()
+    {
+        $this->status = 0;
+        $this->save();
+    }
+
+    public function toggleStatus($value)
+    {
+        if ($this->status == 0) {
+            return $this->allow();
+        }
+
+        return $this->disAllow();
+
+    }
+
+    public function remove()
+    {
+        $this->delete();
+    }
 }
